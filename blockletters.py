66cd410019e730d8e9beb4c54c4850cca6cb5480
Jose Pereira <129305728+yaboymylo@users.noreply.github.com>
2023-03-29 19:32:21 +0100

    Add files via upload

---
+++ blockletters.py
@@ -0,0 +1,11 @@
+# Some Codes From The Hello World Module (Codecademy)
+
+# Block Letters Project
+
+print("JJJJJ   PPPP ")
+print("  J     P   P")
+print("  J     P   P")
+print("  J     PPPP ")
+print("J J     P    ")
+print("J J     P    ")
+print(" JJ     P    ")
